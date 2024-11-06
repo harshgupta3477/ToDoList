465564fa91aec065ac3bb3853721ce3acec7f019
harshgupta3477 <harsh.gupta@edureka.co>
2024-11-06 18:21:52 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from "react";
+import TodoApp from "./components/TodoApp";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <TodoApp />
+        </div>
+    );
+}
+
+export default App;
